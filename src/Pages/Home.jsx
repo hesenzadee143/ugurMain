da20ded15dcbdbabc215ea29df0436d8fde59ef7
n-stepp <xudaverdihesenov@gmail.com>
2023-01-23 22:35:25 +0400

    yeniden

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -18,44 +18,20 @@ import "./Home.css"
 const Home = () => {
 
   return (
-    // <Fragment>
-    //       <Header/>
-    //       <HeroSection/>
-    //       <AboutUs/>
-    //       <Courses/>
-    //       <ChooseUs/>
-    //       <FeatureSection/>
-    //       <FreeCourse/>
-    //       <NewSletter/>
-    //       <Footer/>
-    //       <Communications/>
-    //        {/* <Hero/> */}
-    // </Fragment>
-    <Fragment>
-          <meElement>
-      <h1>Müvəqqəti olaraq saytın  istifadəsi dayandırılmışdır!
-      </h1>
-      <h3>
-        Əlavə məlumat üçün aşağıdaklarla əlaqə  saxlaya bilərsiniz.
-      </h3>
-      <h6>
-          <div> 
-            <h6>
-            <a href="https://wa.me/994515101108">Watsapp üçün buraya klikləyin.</a>
-              Və ya aşağıdakı QR kodunu skan edərək bir başa Watsappa keçid edə bilərsiniz.
-            </h6>
-            <img src={QrWp} alt="" />
-          </div>
-          <div> 
-            <h6>
-              <a href="https://www.instagram.com/hesenzadee143/">Instagram üçün buraya klikləyin.</a>
-              Və ya aşağıdakı QR kodunu skan edərək bir başa instagrama keçid edə bilərsiniz.
-            </h6>
-            <img src={QR} alt="" />
-          </div>
-      </h6>
-    </meElement>
-    </Fragment>
+     <Fragment>
+           <Header/>
+           <HeroSection/>
+           <AboutUs/>
+           <Courses/>
+           <ChooseUs/>
+           <FeatureSection/>
+           <FreeCourse/>
+           <NewSletter/>
+           <Footer/>
+           <Communications/>
+            {/* <Hero/> */}
+     </Fragment>
+
   )
 }
 
