12506d578f67b5f3335e1679855157b3ad63d773
n-stepp <xudaverdihesenov@gmail.com>
2023-02-09 23:11:58 +0400

    aynur x. part 2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,51 +10,51 @@ import Header from '../Components/Header/Header'
 import HeroSection from '../Components/Hero-Section/HeroSection'
 //import { Hero } from '../Components/Hero/Hero'
 import NewSletter from '../Components/NewSletter/NewSletter'
-import QR from "../Assests/Images/QR.jpeg"
-import QrWp from "../Assests/Images/qr-code.png"
+//import QR from "../Assests/Images/QR.jpeg"
+//import QrWp from "../Assests/Images/qr-code.png"
 import "./Home.css"
 
 
 const Home = () => {
 
   return (
-    //  <Fragment>
-    //        <Header/>
-    //       {/* <HeroSection/>*/}
-    //        <ChooseUs/>
-    //        <AboutUs/>
-    //        <Courses/>
-    //        <FeatureSection/>
-    //        <FreeCourse/>
-    //        {/* <NewSletter/> */}
-    //        <Footer/>
-    //        <Communications/>
-    //         {/* <Hero/> */}
-    //  </Fragment>
-    <Fragment>
-    <meElement>
-      <h1>Müvəqqəti olaraq saytın  istifadəsi dayandırılmışdır!</h1>
-      <h3>
-        Əlavə məlumat üçün aşağıdaklarla əlaqə  saxlaya bilərsiniz.
-      </h3>
-      <h6>
-          <div> 
-            <h6>
-            <a href="https://wa.me/994515101108">Watsapp üçün buraya klikləyin.</a>
-              Və ya aşağıdakı QR kodunu skan edərək bir başa Watsappa keçid edə bilərsiniz.
-            </h6>
-            <img src={QrWp} alt="" />
-          </div>
-          <div> 
-            <h6>
-              <a href="https://www.instagram.com/hesenzadee143/">Instagram üçün buraya klikləyin.</a>
-              Və ya aşağıdakı QR kodunu skan edərək bir başa instagrama keçid edə bilərsiniz.
-            </h6>
-            <img src={QR} alt="" />
-          </div>
-      </h6>
-    </meElement>
-    </Fragment>
+      <Fragment>
+            <Header/>
+           {/* <HeroSection/>*/}
+            <ChooseUs/>
+            <AboutUs/>
+            <Courses/>
+            <FeatureSection/>
+            <FreeCourse/>
+            {/* <NewSletter/> */}
+            <Footer/>
+            <Communications/>
+             {/* <Hero/> */}
+      </Fragment>
+    // <Fragment>
+    // <meElement>
+    //   <h1>Müvəqqəti olaraq saytın  istifadəsi dayandırılmışdır!</h1>
+    //   <h3>
+    //     Əlavə məlumat üçün aşağıdaklarla əlaqə  saxlaya bilərsiniz.
+    //   </h3>
+    //   <h6>
+    //       <div> 
+    //         <h6>
+    //         <a href="https://wa.me/994515101108">Watsapp üçün buraya klikləyin.</a>
+    //           Və ya aşağıdakı QR kodunu skan edərək bir başa Watsappa keçid edə bilərsiniz.
+    //         </h6>
+    //         <img src={QrWp} alt="" />
+    //       </div>
+    //       <div> 
+    //         <h6>
+    //           <a href="https://www.instagram.com/hesenzadee143/">Instagram üçün buraya klikləyin.</a>
+    //           Və ya aşağıdakı QR kodunu skan edərək bir başa instagrama keçid edə bilərsiniz.
+    //         </h6>
+    //         <img src={QR} alt="" />
+    //       </div>
+    //   </h6>
+    // </meElement>
+    // </Fragment>
   )
 }
 
